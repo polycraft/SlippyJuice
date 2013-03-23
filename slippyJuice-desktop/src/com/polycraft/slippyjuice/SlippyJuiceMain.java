e992339a908d11f1db122f4b9b2ca66e1603a1ad
Florian <florian.cazalet@free.fr>
2013-03-23 09:22:31 +0100

    update

--- slippyJuice-desktop/src/com/polycraft/slippyjuice/SlippyJuiceMain.java
+++ slippyJuice-desktop/src/com/polycraft/slippyjuice/SlippyJuiceMain.java
@@ -3,7 +3,7 @@ package com.polycraft.slippyjuice;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-public class Main {
+public class SlippyJuiceMain {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "slippyJuice";
