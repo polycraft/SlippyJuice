3aae932166c605401d1258f12ad639e91140fd97
Florian <florian.cazalet@free.fr>
2013-03-23 13:52:08 +0100

    Finalise equipment and items

---
+++ slippyJuice/src/com/polycraft/slippyjuice/player/Player.java
@@ -0,0 +1,5 @@
+package com.polycraft.slippyjuice.player;
+
+public class Player {
+    private PlayerInformation playerInformation;
+}
--- slippyJuice/src/com/polycraft/slippyjuice/scene/Scene.java
+++ slippyJuice/src/com/polycraft/slippyjuice/scene/Scene.java
@@ -1,5 +1,12 @@
 package com.polycraft.slippyjuice.scene;
 
+import com.polycraft.slippyjuice.player.Player;
+import com.polycraft.slippyjuice.scene.ground.Ground;
+import com.polycraft.slippyjuice.scene.vomit.Vomit;
+
 public class Scene {
+    private Player player;
+    private Ground ground;
+    private Vomit vomit;
 
 }
