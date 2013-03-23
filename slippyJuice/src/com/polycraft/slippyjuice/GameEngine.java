e2487fe21fec3d8d9d56545a6e1a99e3f1322189
Florian <florian.cazalet@free.fr>
2013-03-23 09:09:36 +0100

    File added

---
+++ slippyJuice/src/com/polycraft/slippyjuice/GameEngine.java
@@ -0,0 +1,43 @@
+package com.polycraft.slippyjuice;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class GameEngine implements ApplicationListener {
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
