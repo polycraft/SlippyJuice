976b648e1a6ca104b6a1998d7dd1e80b82c6e245
Florian <florian.cazalet@free.fr>
2013-04-27 09:59:49 +0200

    Ajout de la classe HUD

---
+++ slippyJuice/src/com/polycraft/slippyjuice/ui/HUD.java
@@ -0,0 +1,24 @@
+package com.polycraft.slippyjuice.ui;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class HUD extends Stage {
+
+	public HUD() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public HUD(float width, float height, boolean keepAspectRatio,
+			SpriteBatch batch) {
+		super(width, height, keepAspectRatio, batch);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HUD(float width, float height, boolean keepAspectRatio) {
+		super(width, height, keepAspectRatio);
+		// TODO Auto-generated constructor stub
+	}
+
+}
